7b8110fc117ebc85cf36fc00a8f11a1eaae9f5f9
Soger1 <98477507+Soger1@users.noreply.github.com>
2022-03-30 20:23:42 +0100

    My Attempt to start organising my classes corretly

---
+++ java/src/c20322553/Shape.java
@@ -0,0 +1,22 @@
+package c20322553;
+import java.util.ArrayList;
+public class Shape {
+
+        float x;
+        float y;
+        float size;
+        float op;
+        int points;
+        float z;
+        int type;
+
+        public Shape(float x, float y, float size, int points, float z, int type){
+            this.x = x;
+            this.y = y;
+            this.size = size;
+            this.points = points;
+            this.z = z;
+            this.type = type;
+        }
+    
+}
