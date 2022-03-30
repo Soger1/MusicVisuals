640fcbaa01dc97d89980cd354698667efccb99be
Soger1 <98477507+Soger1@users.noreply.github.com>
2022-03-30 17:49:33 +0100

    Updated timer.
    Timer now works independent of frame rate, also made render-list cleaner more aggressive

--- java/src/ie/tudublin/sequencer.java
+++ java/src/ie/tudublin/sequencer.java
@@ -2,6 +2,8 @@ package ie.tudublin;
 import ie.tudublin.Visual;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.time.*;
+
 
 public class sequencer extends Visual {
 
@@ -49,6 +51,9 @@ public class sequencer extends Visual {
     float boxSize = 0;
     float split = 0;
     float rotor = 0;
+    LocalTime present = LocalTime.now();
+    int Secnow = present.getSecond();
+    int Seccheck = present.getSecond();
 
     public void sunrays() //72 degress
     {
@@ -74,6 +79,22 @@ public class sequencer extends Visual {
             rotor++;
         }
         
+    }
+
+    public int Timer(int cursec)
+    {
+        int out;
+        if (Seccheck != cursec)
+        {
+            out = 1;
+            Seccheck = cursec; 
+        }
+        else
+        {
+            out = 0;
+        }
+        
+        return out;
     }
 
     public int getRandom (int min, int max){
@@ -106,7 +127,7 @@ public class sequencer extends Visual {
                 pushMatrix();
                 fill(255);
                 noStroke();
-                shape.z = shape.z + 3f;
+                shape.z = shape.z + 30f;
                 translate(shape.x, shape.y, shape.z);
                 if (shape.size > 3){
                     shape.size = 1;
@@ -160,12 +181,13 @@ public class sequencer extends Visual {
     public void setup(){
         colorMode(HSB);
         noCursor();
-
+        textSize(height / 6);
         
         setFrameSize(256);
 
         startMinim();
         loadAudio("source.mp3");
+
         getAudioPlayer().cue(0);
         getAudioPlayer().play();
 
@@ -173,6 +195,7 @@ public class sequencer extends Visual {
 
     public void draw()
     {
+        float[] bands = getSmoothedBands();
         calculateAverageAmplitude();
         try
         {
@@ -224,51 +247,65 @@ public class sequencer extends Visual {
             chorusbase();
             break;
         }
-        if (frameCount % 60 == 0){
-            time++;
-        }
-        if (time <= 26){
+        present = LocalTime.now();
+        Secnow = present.getSecond();
+        time = time + Timer(Secnow);
+        if (time <= 27){
             stage = 0;
         }
-        if (time > 26 & time <= 64){
+        if (time > 27 & time <= 63){
             stage = 1;
         }
-        if (time > 64 & time <= 84){
+        if (time > 63 & time <= 82){
             stage = 2;
         }
-        if (time > 84 & time <= 131){
+        if (time > 82 & time <= 129){
             stage = 3;
         }
-        if (time > 131 & time <= 167){
+        if (time > 128 & time <= 164){
             stage = 4;
         }
-        if (time > 167 & time <= 206){
+        if (time > 164 & time <= 201){
             stage = 5;
         }
-        if (time > 206 & time <= 243){
+        if (time > 201 & time <= 238){
             stage = 6;
         }
-        if (time > 243){
+        if (time > 238){
             stage = 7;
         }
-        if(time > 163 & time <= 167){
-            textSize(height / 6);
+        if(time > 158 & time <= 162){
             fill(255);
             text("IN THE SILENCE", width / 7, height / 3);
-            if (time > 165)
+            if (time > 1610)
             {
                 text("IN THE SILENCE", width / 7, (height / 3) * 2);
             }
             noFill();
         }
-        if ( Renderlist.size() > 200){
+        if ( Renderlist.size() > 900){
             Renderlist.remove(1);
             Renderlist.remove(2);
             Renderlist.remove(3);
             Renderlist.remove(4);
             Renderlist.remove(5);
+            Renderlist.remove(6);
+            Renderlist.remove(7);
+            Renderlist.remove(8);
+            Renderlist.remove(9);
+            Renderlist.remove(10);
+
         }
-        
+        //Debug Stuff
+        //textSize(12);
+        //text("Renderlist size:" + Renderlist.size(), 30, 30);
+        //text("Time:" + time, 30, 45);
+        //text("Time_Debug:" + Secnow, 30, 60);
+
+        //for(int i = 0 ; i < bands.length ; i++)
+        //{
+        //    text("Band_" + i + ":" + bands[i], 150, 30 + 15 * i);
+        //}
     }
 
 }
