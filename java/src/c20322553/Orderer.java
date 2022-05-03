37e0e6f050dec5ab25b180fa482fb915a1426639
Ethan holohan <c20322553@mytudublin.ie>
2022-05-03 18:10:47 +0100

    Added good inheritence and made the shape class more significant

--- java/src/c20322553/Orderer.java
+++ java/src/c20322553/Orderer.java
@@ -1,5 +1,4 @@
 package c20322553;
-import c20322553.Shape;
 
 import ie.tudublin.Visual;
 import ie.tudublin.*;
@@ -12,35 +11,35 @@ public class Orderer extends Visual {
 
     
     ArrayList<Shape> Renderlist = new ArrayList<>();
-
-    void polygon(float x, float y, float radius, int npoints) { // draws polygons with x number of points
-        float angle = TWO_PI / npoints;
-        beginShape();
-        for (float a = 0; a < TWO_PI; a += angle) {
-          float sx = x + cos(a) * radius;
-          float sy = y + sin(a) * radius;
-          vertex(sx, sy, 0);
-        }
-        endShape(CLOSE);
-    }
-
-    public void createShape(int x, int y, int size, int points, int z, int type, int life){ // adds shapes to the render list
-        Renderlist.add(new Shape(x,y,size,points,z,type,life));
-    }
     
     float angle = 0;
-    int stage = 0;
-    int time = 0;
+    private int stage = 0;
+    private int time = 0;
     float boxSize = 0;
     float split = 0;
     float rotor = 0;
     LocalTime present = LocalTime.now();
     int Secnow = present.getSecond();
     int Seccheck = present.getSecond();
-    int backdrop = 240;
+    private int backdrop = 240;
     int time_changed = 0;
-    int sweep_num = 0;
-    float sweep_place = -100;
+    private int sweep_num = 0;
+    public float sweep_place = -100;
+
+    public int getTime() {
+        return this.time;
+    }
+
+    public int getsweepnum() {
+        return this.sweep_num;
+    }
+    public int getbackdrop() {
+        return this.backdrop;
+    }
+
+    public int getstage() {
+        return this.stage;
+    }
 
     public void sunrays() //72 degress
     {
@@ -94,182 +93,24 @@ public class Orderer extends Visual {
         {
             int rx = getRandom(0, width);
             int ry = getRandom(0, height);
-            createShape(rx,ry,(int) bands[i] / 10,0,-300,1, 0);
-        }
+            Renderlist.add(new Star(rx,ry,(int) bands[i] / 10, -300,this));
     }
-
-    int bubble(float x, float y, float size, int points, int life){
-        float max = 0;
-        
-        if (time > 255){
-            max = 500;
-        }
-        else{
-            max = 1000 * getSmoothedAmplitude();
-        }
-
-        if (size > max || life <= 0){
-            int sides = points * 2;
-            float px = x;
-            float py = y - size; 
-            for(int i = 0 ; i <= sides ; i ++)
-            {
-                float r = (i % 2 == 0) ? size : size / 2; 
-                // float r = radius;
-                float theta = map(i, 0, sides, 0, TWO_PI);
-                float newx = x + sin(theta) * r;
-                float newy = y - cos(theta) * r;
-                
-                //circle(x, y, 20);
-                line(px, py, newx, newy);
-                px = newx;
-                py = newy;
-            }
-            int decrease = (int)(8 - 50 * getSmoothedAmplitude());
-            if (decrease < 1){
-                decrease = 1;
-            }
-            return decrease;
-        }
-        else{
-            circle(x, y, size);
-            return 0;
-        }
     }
     
     public void creSweeper(){
         if(frameCount % 360 == 0){
             sweep_num ++;
-            createShape(0,0,0,0,0,5,sweep_num);
+            Renderlist.add(new sweeper(0,sweep_num,5, stage,this));
         }
-    }
-
-    public void drawSweeper(float x){
-        float position = (x * width/ 360);
-        line(position, 0, position, height);
-    }
-    
-    public void vertrect(float x, float y, float size){
-        rect(x, y, size, height);
-    }
-
-    public void horirect(float x, float y, float size){
-        rect (x, y, width, size);
     }
 
     public void render(){ // renders all shapes in render list
         for (Shape shape: Renderlist){
-            if (shape.type == 0){
-            pushMatrix();
-            translate(width / 2,height / 2, -200);
-            rotate( frameCount / 200.0f);
-            polygon(shape.x, shape.y, shape.size, shape.points); // drawing polygons
-            popMatrix();
-            shape.size = shape.size + 16f;
-            }
-            if (shape.type == 1){
-                pushMatrix();
-                fill(255);
-                noStroke();
-                shape.z = shape.z + 30f;
-                translate(shape.x, shape.y, shape.z);
-                if (shape.size > 3){
-                    shape.size = 1;
-                }
-                circle(shape.x, shape.y, shape.size);
-                noFill();
-                stroke(map(getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
-                popMatrix(); 
-            }
-            if (shape.type == 2){
-                setColour(shape.type);
-                strokeWeight(2);
-                pushMatrix();
-                if(shape.x - shape.size <= ((sweep_place) * width/360) && shape.x + shape.size >= (sweep_place * width/ 360)){
-                    shape.life = 0;
-                }
-                if(shape.life > 0){
-                    shape.life = shape.life - bubble(shape.x, shape.y, shape.size, shape.points, shape.life);
-                    if((shape.y - shape.size/2) >= (height - height/backdrop)){
-                        shape.size = shape.size + 1;
-                    }
-                    else{
-                        shape.life = 0;
-                    }
-                }
-                else if(stage == 7){
-                    setColour(shape.type);
-                    fill(0,0,0);
-                    pushMatrix();
-                    bubble(shape.x, shape.y, shape.size, shape.points, shape.life);
-                    popMatrix();
-                    noFill();
-                }
-                popMatrix();
-            }
-            if (shape.type == 3){
-                setColour(shape.type);
-                pushMatrix();
-                vertrect(shape.x, shape.y, shape.size);
-                popMatrix();
-                noFill();
-            }
-            if (shape.type == 4){
-                setColour(shape.type);
-                pushMatrix();
-                horirect(shape.x, shape.y, shape.size);
-                popMatrix();
-                noFill();
-            }
-            if (shape.type == 5){
-                setColour(shape.type);
-                pushMatrix();
-                drawSweeper(shape.x);
-                popMatrix();
-                strokeWeight(2);
-                shape.x ++;
-                if(shape.life == sweep_num){
-                    sweep_place = (shape.x - 2);
-                }
-            }
+            shape.render();
         }
     }
 
-    public void setColour(int type){
-        if(type == 2){
-            switch(stage){
-                case 0:
-                    stroke(255,255,0);
-                    break;
-                case 5:
-                    stroke(255,0,0);
-                    break;
-                case 7:
-                    stroke(0,0,0);
-                    break;
-            }
-        }
-        if(type == 3 || type == 4){
-            switch(stage){
-                case 0:
-                    fill(255,165,0);
-                    stroke(255,165,0);
-                    break;
-                case 5:
-                    stroke(191,64,191);
-                    fill(191,64,191);
-                    break;
-                case 7:
-                    stroke(0,252,0);
-                    fill(0,252,0);
-                    break;
-            }
-        }
-        if(type == 5){
-            stroke(255,255,255);
-            strokeWeight(15);
-        }
-    }
+    
 
     public void chorusbase(){
         stroke(map(getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
@@ -283,7 +124,7 @@ public class Orderer extends Visual {
             {
                 poi = poi + 2;
             }
-            createShape(0,0,1,poi,0,0, 0);
+            Renderlist.add(new Polygon(0,0,1, poi, this));
         }
 
         pushMatrix();
@@ -302,8 +143,8 @@ public class Orderer extends Visual {
         render();
 
         if (backdrop > 1){
-            createShape(width/2, 0, width/15, 0, 0, 3, 0);
-            createShape(0, height - 2*(height/backdrop), height, 0, 0, 4, 0);
+            Renderlist.add(new v_wall(width/2,0,width/15,this));
+            Renderlist.add(new h_wall(0,height - 2*(height/backdrop),height,this));
             backdrop = backdrop - 1;
         }
         if (frameCount % 5 == 0){
@@ -318,7 +159,8 @@ public class Orderer extends Visual {
                 poi = 15;
             }
 
-            createShape(xposition, yposition, 1, poi, 0, 2, 8);
+
+            Renderlist.add(new bubble(xposition,yposition,1,poi,800,this,2));
         }
     }
 
--- java/src/c20322553/Shape.java
+++ java/src/c20322553/Shape.java
@@ -1,24 +1,293 @@
 package c20322553;
-import java.util.ArrayList;
-public class Shape {
 
-        float x;
-        float y;
-        float size;
-        float op;
-        int points;
-        float z;
-        int type;
-        int life;
 
-        public Shape(float x, float y, float size, int points, float z, int type, int life){
-            this.x = x;
-            this.y = y;
-            this.size = size;
-            this.points = points;
-            this.z = z;
-            this.type = type;
-            this.life = life;
+
+public class Shape extends Orderer {
+
+    public void setColour(int type, Orderer or){
+        if(type == 2){
+            switch(getstage()){
+                case 0:
+                    or.stroke(255,255,0);
+                    break;
+                case 5:
+                    or.stroke(255,0,0);
+                    break;
+                case 7:
+                    or.stroke(0,0,0);
+                    break;
+            }
         }
+        if(type == 3 || type == 4){
+            switch(getstage()){
+                case 0:
+                    or.fill(255,165,0);
+                    or.stroke(255,165,0);
+                    break;
+                case 5:
+                    or.stroke(191,64,191);
+                    or.fill(191,64,191);
+                    break;
+                case 7:
+                    or.stroke(0,252,0);
+                    or.fill(0,252,0);
+                    break;
+            }
+        }
+        if(type == 5){
+            or.stroke(255,255,255);
+            or.strokeWeight(15);
+        }
+    }
+}
+
+class Polygon extends Shape
+{
+    float x;
+    float y;
+    float size;
+    float op;
+    int points;
+    Orderer or;
+
+
+    public Polygon(float x, float y, float size, int points, Orderer or){
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.points = points;
+        this.or = or;
+    }
+
+    public void render(){
+        or.pushMatrix();
+        or.translate(or.width / 2,or.height / 2, -200);
+        or.rotate( or.frameCount / 200.0f);
+        polygon(x, y, size, points); // drawing polygons
+        or.popMatrix();
+        size = size + 16f;
+    }
+
+    public void polygon(float x, float y, float radius, int npoints) { // draws polygons with x number of points
+        float angle = TWO_PI / npoints;
+        or.beginShape();
+        for (float a = 0; a < TWO_PI; a += angle) {
+            float sx = x + cos(a) * radius;
+            float sy = y + sin(a) * radius;
+            or.vertex(sx, sy, 0);
+        }
+        or.endShape(CLOSE);
+    }
+}
+
+class Star extends Shape
+{
+    float x;
+    float y;
+    float size;
+    float op;
+    float z;
+    Orderer or;
+
+    public Star(float x, float y, float size, float z, Orderer or){
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.z = z;
+        this.or = or;
+    }
+
+    public void render(){
+        or.pushMatrix();
+        or.fill(255);
+        or.noStroke();
+        z = z + 30f;
+        or.translate(x, y, z);
+        if (size > 3)
+        {
+            size = 1;
+        }
+        or.circle(x, y, size);
+        or.noFill();
+        or.stroke(map(or.getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
+        or.popMatrix(); 
+    }
+}
+
+class bubble extends Shape
+{
+    float x;
+    float y;
+    float size;
+    float op;
+    int points;
+    int life;
+    int type;
+    Orderer or;
+
+    public bubble(float x, float y, float size, int points, int life, Orderer or, int type){
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.points = points;
+        this.life = life;
+        this.or = or;
+        this.type = type;
+    }
+
+    public void render(){
+        or.stroke(255,255,0);
+        or.strokeWeight(2);
+        or.pushMatrix();
+        if(x - size <= ((sweep_place) * or.width/360) && +size >= (sweep_place * or.width/ 360)){
+           life = 0;
+        }
+        if(life > 0){
+            life = life - bubbles(x, y, size, points, life);
+            if((y - size/2) >= (or.height - or.height/getbackdrop())){
+                size ++;
+            }
+            //else{
+            //    life = 0;
+            //}
+        }
+        else if( getstage() == 7){
+            or.stroke(255,255,0);
+            or.fill(0,0,0);
+            or.pushMatrix();
+            bubbles(x, y, size, points, life);
+            or.popMatrix();
+            or.noFill();
+        }
+        or.popMatrix();
+    }
+
+    public int bubbles(float x, float y, float size, int points, int life){
+        float max = 0;
+        
+        if (getTime() > 255){
+            max = 500;
+        }
+        else{
+            max = 1000 * getSmoothedAmplitude();
+        }
+
+        if (size > max || life <= 0){
+            int sides = points * 2;
+            float px = x;
+            float py = y - size; 
+            for(int i = 0 ; i <= sides ; i ++)
+            {
+                float r = (i % 2 == 0) ? size : size / 2; 
+                // float r = radius;
+                float theta = map(i, 0, sides, 0, TWO_PI);
+                float newx = x + sin(theta) * r;
+                float newy = y - cos(theta) * r;
+                
+                //circle(x, y, 20);
+                or.line(px, py, newx, newy);
+                px = newx;
+                py = newy;
+            }
+            int decrease = (int)(8 - 50 * getSmoothedAmplitude());
+            if (decrease < 1){
+                decrease = 1;
+            }
+            return decrease;
+        }
+        else{
+            or.circle(x, y, size);
+            return 0;
+        }
+    }
+}
+
+class v_wall extends Shape
+{
+    float x;
+    float y;
+    float size;
+    Orderer or;
+
+    public v_wall(float x, float y, float size, Orderer or){
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.or = or;
+    }
+
+    public void render(){
+
+        or.fill(255,165,0);
+        or.stroke(255,165,0);
+        vertrect(x, y, size);
+        or.noFill();
+    }
+
+    public void vertrect(float x, float y, float size){
+        or.rect(x, y, size, or.height);
+    }
+}
+
+class h_wall extends Shape
+{
+    float x;
+    float y;
+    float size;
+    Orderer or;
+
+    public h_wall(float x, float y, float size, Orderer or){
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.or = or;
+    }
+
+    public void render(){
+        or.fill(255,165,0);
+        or.stroke(255,165,0);
+        horirect(x, y, size);
+        or.noFill();
+    }
+
+    public void horirect(float x, float y, float size){
+        or.rect(x, y, or.width, size);
+    }
+}
+
+class sweeper extends Shape
+{
+    float x;
+    float life;
+    int type;
+    int stage;
+    Orderer or;
+
+    public sweeper(float x, float life, int type, int stage, Orderer or)
+    {
+        this.x = x;
+        this.life = life;
+        this.type = type;
+        this.or = or;
+    }
+
+    public void render(){
+        setColour(type, or);
+        or.pushMatrix();
+        drawSweeper(x);
+        or.popMatrix();
+        or.strokeWeight(2);
+        x ++;
+        if(life == getsweepnum()){
+            sweep_place = (x - 2);
+        }
+    }
+
+    public void drawSweeper(float x){
+        float position = (x * width/ 360);
+        or.line(position, 0, position, height);
+    }
+
+    
     
 }
